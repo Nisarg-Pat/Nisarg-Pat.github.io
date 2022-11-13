37e803640bb1c13b68a9215d82ab691db08ed937
Nisarg Patel <patel321nisarg@gmail.com>
2022-11-13 13:04:09 -0500

    Updated About page

--- src/components/HomeScreen/HomeScreenComponent.js
+++ src/components/HomeScreen/HomeScreenComponent.js
@@ -1,17 +1,11 @@
 import React from "react";
 
+import about from "../../data/about.json"
+
 const HomeScreenComponent = () => {
     return (
         <>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed egestas egestas fringilla phasellus faucibus scelerisque. Diam vulputate ut pharetra sit amet aliquam id. Odio aenean sed adipiscing diam. Vel risus commodo viverra maecenas accumsan lacus. In est ante in nibh mauris cursus mattis. Faucibus in ornare quam viverra. Auctor neque vitae tempus quam pellentesque nec nam aliquam. Aliquet bibendum enim facilisis gravida neque convallis. Interdum posuere lorem ipsum dolor sit amet consectetur. Viverra accumsan in nisl nisi scelerisque eu ultrices vitae auctor. Diam sit amet nisl suscipit adipiscing bibendum est ultricies integer. Egestas sed tempus urna et pharetra pharetra massa. Proin sagittis nisl rhoncus mattis rhoncus urna.
-            </p>
-            <p>
-                Cursus vitae congue mauris rhoncus aenean vel elit. Vitae tortor condimentum lacinia quis vel eros donec. Leo duis ut diam quam nulla porttitor massa id. Eget magna fermentum iaculis eu non diam phasellus vestibulum lorem. Eget egestas purus viverra accumsan in nisl nisi scelerisque eu. Ipsum dolor sit amet consectetur adipiscing. Accumsan lacus vel facilisis volutpat est. Integer malesuada nunc vel risus commodo viverra maecenas accumsan. Tincidunt lobortis feugiat vivamus at augue eget. Elit at imperdiet dui accumsan sit. Condimentum lacinia quis vel eros. Habitasse platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper dignissim. Duis at tellus at urna condimentum mattis pellentesque id nibh. Odio aenean sed adipiscing diam donec adipiscing tristique. Nec ultrices dui sapien eget mi proin sed. Vel orci porta non pulvinar neque laoreet. Hac habitasse platea dictumst vestibulum rhoncus est pellentesque elit. Ipsum a arcu cursus vitae congue mauris rhoncus aenean vel. Ultrices sagittis orci a scelerisque purus semper eget. Pellentesque dignissim enim sit amet venenatis urna cursus eget nunc.
-            </p>
-            <p>
-                Integer vitae justo eget magna. Pretium aenean pharetra magna ac placerat vestibulum. Ultrices dui sapien eget mi proin sed libero enim. Suspendisse ultrices gravida dictum fusce. Quisque non tellus orci ac. Elementum sagittis vitae et leo duis ut diam quam. Nunc vel risus commodo viverra maecenas accumsan lacus. Eu turpis egestas pretium aenean pharetra magna ac placerat vestibulum. Semper eget duis at tellus at urna condimentum mattis. Lectus magna fringilla urna porttitor. Pulvinar elementum integer enim neque volutpat ac tincidunt vitae semper. Sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula ipsum.
-            </p>
+        {about.details.map((item, key) => <p> {item} </p>)}
         </>
     )
 }
