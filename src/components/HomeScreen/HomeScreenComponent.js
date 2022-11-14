c1d1462c7c5152a95b4f63a1e8c9ce23bc86a082
Nisarg Patel <patel321nisarg@gmail.com>
2022-11-14 14:39:24 -0500

    Adding leetcode symbol and profile icon

--- src/components/HomeScreen/HomeScreenComponent.js
+++ src/components/HomeScreen/HomeScreenComponent.js
@@ -5,7 +5,7 @@ import about from "../../data/about.json"
 const HomeScreenComponent = () => {
     return (
         <>
-            <img src={"/images/profile.png"} alt={"Profile Image"} className={"al-profile-img"}/>
+            <img src={"/images/profile.jpg"} alt={"Profile Image"} className={"al-profile-img"}/>
             <div className={"al-profile-disc"}>
                 {about.details.map((item, key) => <p> {item} </p>)}
             </div>
